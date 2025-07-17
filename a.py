9e3cd44ca2d576512e0eedfef6893b5320da9d8b
Matin.Am <matin.amani101013@gmail.com>
2025-07-17 21:01:22 +0330

    reverse function

--- a.py
+++ a.py
@@ -1,13 +1,7 @@
 
 
-def fibonachi(n):
-    if n < 0 :
-        raise Exception("minus numbers are not allowed")
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1 
-    
-    return fibonachi(n-1) + fibonachi(n-2)
 
-print(fibonachi(10))
+def revers(name):
+    return name[::-1]
+
+ 
