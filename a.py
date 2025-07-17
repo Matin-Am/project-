e79b581bdad2d255ce42495ffe6a56ba4c2b8a7d
Matin.Am <matin.amani101013@gmail.com>
2025-07-17 19:53:03 +0330

    .

--- a.py
+++ a.py
@@ -1,11 +1,13 @@
 
 
-
-def fac(n):
+def fibonachi(n):
     if n < 0 :
-        raise Exception("minus numbers not allowed ! ")
-    if n == 0 or n == 1:
+        raise Exception("minus numbers are not allowed")
+    if n == 0:
+        return 0
+    if n == 1:
         return 1 
-    return n * fac(n-1)
+    
+    return fibonachi(n-1) + fibonachi(n-2)
 
-print(fac(3))
+print(fibonachi(10))
